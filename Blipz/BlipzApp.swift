ca4fdeb67c82aebda32228b4ec1056c349e6e385
Shayan Manoharan <shayanmanoharan@gmail.com>
2026-06-01 23:03:12 -0400

    Initial Commit

---
+++ Blipz/BlipzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlipzApp.swift
+//  Blipz
+//
+//  Created by Shayan Manoharan on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct BlipzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlipzTests/BlipzTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlipzTests.swift
+//  BlipzTests
+//
+//  Created by Shayan Manoharan on 6/1/26.
+//
+
+import Testing
+@testable import Blipz
+
+struct BlipzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
